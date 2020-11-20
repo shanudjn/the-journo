697e00123a56ba3313f820683891acf9a6ce2650
Md Shahazad Uddin <shanudjn@gmail.com>
2020-11-20 15:50:30 +0530

    added the client and server beginner code

---
+++ server/index.js
@@ -0,0 +1,21 @@
+//the import statements are the ES6 syntax for node. Need to add  "type": "module" in package.json file
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shahazad:shahazad1234@cluster0.wwwc4.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
+mongoose.set('useFindAndModify') // disabling warnings
