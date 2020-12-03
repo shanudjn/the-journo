c7ab7095b107225393ba85e96bc19a335e4ca38d
Md Shahazad Uddin <shanudjn@gmail.com>
2020-12-03 21:16:33 +0530

    add styles for the components

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './styles.css';
 
 const Post = () => {
     return (
-        <h1>Post</h1>
+        <div className="post">
+            <h4>Post</h4>
+            <small><i>26th December</i></small>
+            <p>
+                Jhsajdhjkahdkahdahdkajdhakdhad
+            </p>
+            <p>
+                afjhadkfhajfhakjkjdhdjakdhsdjkahdkas
+            </p>
+
+        </div>
     )
 }
 
