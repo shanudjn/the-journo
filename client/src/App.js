be2e247fc13d22a5078ff501d50db5f1a938b33a
Md Shahazad Uddin <shanudjn@gmail.com>
2020-12-04 14:33:43 +0530

    changed background for better UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,37 @@
 import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid, ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { Container, Typography, Grid, } from '@material-ui/core';
 import './App.css';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
 
-const theme = createMuiTheme({
-    typography: {
-        fontFamily: [
-            'Brush Script MT', 'Brush Script Std', 'cursive'
-        ].join(','),
-    },
-});
+// const theme = createMuiTheme({
+//     typography: {
+//         fontFamily: [
+//             'Brush Script MT', 'Brush Script Std', 'cursive'
+//         ].join(','),
+//     },
+// });
 
 const App = () => {
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <ThemeProvider theme={theme}><Typography variant="h2" align="center">The Journo</Typography></ThemeProvider>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItem="stretch" spacing={4}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form />
-                        </Grid>
 
+            <h1><strong>The Journo 📓</strong></h1>
+
+            <Container>
+                <Grid container justify="space-between" alignItem="stretch" spacing={4}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
                     </Grid>
-                </Container>
-            </Grow>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+
+                </Grid>
+            </Container>
+
 
         </Container>
     )
